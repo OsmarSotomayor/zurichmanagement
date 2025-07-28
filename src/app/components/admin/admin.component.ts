87ecdc8ca6e809066b8b95cec1790524d04761c6
Omar Sotomayor <oscaromarfloressotomayor@gmail.com>
2025-07-27 20:27:11 -0600

    agrego panel para administracion

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../auth/pages/services/auth.service';
 
 @Component({
   selector: 'app-admin',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
 export class AdminComponent {
 
+   private authService = inject(AuthService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.checkAdminRole();
+  }
+
+  private checkAdminRole(): void {
+    if (this.authService.getRole() !== 'Admin') {
+      this.router.navigate(['/login']); // Redirige si no es admin
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
